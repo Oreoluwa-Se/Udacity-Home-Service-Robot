bb4f107de328b0a487550cff37ad753c449a6550
Oreoluwa Seweje <67057368+Oreoluwa-Se@users.noreply.github.com>
2020-08-20 19:26:18 -0400

    Create add_marker.sh

---
+++ catkin_ws/src/scripts/add_marker.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+#Extracting working directory from the test base
+cmd=$(pwd)
+cd ..
+cd ..
+Base=$(pwd)
+
+#Deploy source file and launch turtlebot_world.launch
+xterm -e "cd ${Base} && source devel/setup.bash && roslaunch turtlebot_gazebo turtlebot_world.launch" &
+sleep 5
+
+#Deploy amcl demo from turtlebot simulator package
+xterm -e "cd ${Base} && source devel/setup.bash && roslaunch turtlebot_gazebo amcl_demo.launch" &
+sleep 5
+
+#Deploy view_navigation.launch
+xterm -e "cd ${Base} && source devel/setup.bash && roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+
+#Deploy add_markers.launch
+xterm -e "cd ${Base} && source devel/setup.bash && roslaunch add_markers markers.launch"
+
+
+
+
+ 
